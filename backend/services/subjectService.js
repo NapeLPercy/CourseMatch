232fa269fe8d45e3cc0422ece6f86d3b81b6cdc7
Lekoloane Nape Percy <lekoloanenape5@gmail.com>
2026-04-08 07:31:15 +0200

    fix

--- backend/services/subjectService.js
+++ backend/services/subjectService.js
@@ -5,7 +5,7 @@ dotenv.config();
 const subjectModel = require("../models/Subject");
 const { insertStudentEndorsement } = require("./studentService");
 const SubjectsSanitizer = require("./subjectsSanitizer");
-const MatricEndorsement = require("./matrixEndorsement");
+const MatricEndorsement = require("./matrixEndorsement.js");
 
 /* 1 Clean subjects
 2 Compute matric endorsement for 7 clean subjects
